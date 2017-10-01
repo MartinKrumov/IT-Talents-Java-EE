bd803d8ced41243717af3be8d5bb6103facdd48f
MartinKrumov <martin_krumov@abv.bg>
2017-10-01 13:53:13 +0300

    Delete Drumms.java

--- src/instruments/Drumms.java
+++
@@ -1,9 +0,0 @@
-package instruments;
-
-public class Drumms extends Instrument{
-
-	public Drumms(String name, double price, int quantity) {
-		super(name, price, quantity, InstrumentType.UDARNI);
-	}
-
-}
