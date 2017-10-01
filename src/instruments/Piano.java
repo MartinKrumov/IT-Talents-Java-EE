a569ead3d3742771549bc9fa58d820f8a119a37f
MartinKrumov <martin_krumov@abv.bg>
2017-10-01 13:52:55 +0300

    Delete Piano.java

--- src/instruments/Piano.java
+++
@@ -1,9 +0,0 @@
-package instruments;
-
-public class Piano extends Instrument{
-
-	public Piano(String name, double price, int quantity) {
-		super(name, price, quantity, InstrumentType.KLAVIRNI);
-	}
-
-}
