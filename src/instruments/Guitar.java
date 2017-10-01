f004b9756815e57539575dc1cb2b492711f5a753
MartinKrumov <martin_krumov@abv.bg>
2017-10-01 13:53:08 +0300

    Delete Guitar.java

--- src/instruments/Guitar.java
+++
@@ -1,9 +0,0 @@
-package instruments;
-
-public class Guitar extends Instrument {
-
-	public Guitar(String name, double price, int quantity) {
-		super(name, price, quantity, InstrumentType.STRUNNI);
-	}
-
-}
