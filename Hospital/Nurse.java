f1d363f1249faa3a8e871281b4348edd169ed944
Martin <martin_krumov@abv.bg>
2017-10-09 12:01:33 +0300

    Initial

---
+++ Hospital/Nurse.java
@@ -0,0 +1,26 @@
+package hospital;
+
+public class Nurse extends Person {
+
+	private int exp;
+	private Department dep;
+	
+	public Nurse(String firstName, String lastName, String phone, int exp, Department dep) {
+		super(firstName, lastName, phone);
+		this.exp = exp;
+		this.dep = dep;
+	}
+
+	public Department getDep() {
+		return dep;
+	}
+	
+	public void giveMeds(){
+		for(Room r : dep.getRooms()){
+			for(Patient p : r.getOccupants()){
+				p.receiveMeds(this);
+			}
+		}
+	}
+	
+}
